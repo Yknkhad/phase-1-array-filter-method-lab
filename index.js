ab97c5d6652a31e9a76b0f4641fa8fed000e175b
Ahmed <ahmed@Ahmeds-MacBook-Pro.local>
2024-12-04 14:50:08 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution here
+function findMatching(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+}
+function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.startsWith(query));
+}
+function matchName(drivers, query) {
+    return drivers.filter(driver => driver.name === query);
+}
+const drivers = [
+    { name: "mike", hometown: "england" },
+    { name: "alex", hometown: "San Francisco" },
+    { name: "Sally", hometown: "Los Angeles" },
+    { name: "steve", hometown: "Chicago" },
+    { name: "john", hometown: "Boston" }
+];
+
+console.log(findMatching(["john", "alex", "Sally", "steve", "john"], "john"));
+console.log(fuzzyMatch(["john", "alex", "Sally", "steve", "john"], "Sa"));
+console.log(matchName(drivers, "john"));
+
+
+
+
+
+
+
